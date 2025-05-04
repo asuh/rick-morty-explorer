7527b55bf78c14425fec903f6f84abd26c7d501c
Micah <micah@asuh.com>
2025-05-04 12:31:34 -0400

    Add React Router, create List and Details pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import type React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CharactersList from './pages/CharactersList'
+import CharactersDetails from './pages/CharactersDetails'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<CharactersList />} />
+        <Route path="/characters/:id" element={<CharactersDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ src/pages/CharactersDetails.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+const CharactersDetails: React.FC = () => {
+  return (
+    <div>
+      <h1>Character Details</h1>
+    </div>
+  );
+};
+
+export default CharactersDetails;
